8ebbbcce6bf54306f852caa0604e5861ff383242
BearWu <BearWu@DESKTOP-JA1OFEM>
2016-07-12 19:19:42 +0800

    第二個測試案例 - 兩本不同的書 共190元 紅燈

--- ShippingCartTests/ShippingCartTests/HarryPotterShippingCartTests.cs
+++ ShippingCartTests/ShippingCartTests/HarryPotterShippingCartTests.cs
@@ -26,5 +26,24 @@ namespace ShippingCartTests
             double expected = 100d;
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void CalculateFee_2diffbooks_for_200()
+        {
+            //Arrange
+            var target = new HarryPotterShippingCart();
+            List<Book> booklist =
+                new List<Book>
+            {
+                new Book {Name ="哈利波特第1集",Qty=1,UnitPrice=100 },
+                new Book {Name ="哈利波特第2集",Qty=1,UnitPrice=100 }
+            };
+
+            //Actual
+            double actual = target.CalculateFee(booklist);
+
+            //Assert
+            double expected = 190d;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
