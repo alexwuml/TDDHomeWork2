4fb84a558d68196b44df7ba27a4c6bcf0f62e103
BearWu <BearWu@DESKTOP-JA1OFEM>
2016-07-12 17:52:43 +0800

    First case for 一本書100元

---
+++ ShippingCartTests/ShippingCartTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShippingCartTests
+{
+    [TestClass]
+    public class ShhippingCartTests
+    {
+        [TestMethod]
+        public void CalculateFee_1book_for_100()
+        {
+            //Arrange
+            var target = new HarryPotterShippingCart();
+            List<Book> booklist =
+                new List<Book>
+            {
+                new Book {Name ="",Qty=1,UnitPrice=100 }
+            };
+
+            //Actual
+            double actual = target.CalculateFee(booklist);
+
+            //Assert
+            double expected = 100d;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
